38215bd4024bfd4382c9ee22721fd806302de1dc
LeandroGoCosta <leandro.gocosta@gmail.com>
2021-03-30 23:50:32 -0300

    primeiro commit SpringDataJpa

---
+++ src/main/java/com/inuvens/SprintDataJpa/enums/PerfilEnum.java
@@ -0,0 +1,8 @@
+package com.inuvens.SprintDataJpa.enums;
+
+public enum PerfilEnum {
+	
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+	
+}
---
+++ src/test/java/com/inuves/SpingDataJpa/SpingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inuves.SpingDataJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
